e9f1dcdec95221e367b31364550de8926f9bb1f3
class12 <mansoorihassan409@gmail.com>
2026-05-31 18:08:05 +0500

    6-15 cricketer added

--- app.js
+++ app.js
@@ -9,7 +9,18 @@
   { name: "Babar Azam", country: "Pakistan", debut: 2015, retirement: "Still Playing", age: 30, img: "images/babar.jpg" },
   { name: "Steve Smith", country: "Australia", debut: 2010, retirement: "Still Playing (Test/T20I)", age: 35, img: "images/smith.jpg" },
   { name: "Rohit Sharma", country: "India", debut: 2007, retirement: "Still Playing (ODI/T20I)", age: 38, img: "images/rohit.jpg" },
-  ]
+   { name: "Kane Williamson", country: "New Zealand", debut: 2010, retirement: "Still Playing", age: 35, img: "images/williamson.jpg" },
+  { name: "Joe Root", country: "England", debut: 2012, retirement: "Still Playing", age: 34, img: "images/root.jpg" },
+  { name: "Ben Stokes", country: "England", debut: 2013, retirement: "Still Playing", age: 34, img: "images/stokes.jpg" },
+  { name: "Jasprit Bumrah", country: "India", debut: 2016, retirement: "Still Playing", age: 31, img: "images/bumrah.jpg" },
+  { name: "Shaheen Afridi", country: "Pakistan", debut: 2018, retirement: "Still Playing", age: 25, img: "images/shaheen.jpg" },
+  { name: "Chris Gayle", country: "West Indies", debut: 1999, retirement: 2021, age: 45, img: "images/gayle.jpg" },
+  { name: "Finn Allen", country: "New Zealand", debut: 2021, retirement: "Still Playing", age: 26, img: "images/finnallen.jpg" },
+  { name: "Jofra Archer", country: "England", debut: 2019, retirement: "Still Playing", age: 30, img: "images/archer.jpg" },
+  { name: "Brendon McCullum", country: "New Zealand", debut: 2002, retirement: 2016, age: 43, img: "images/mccullum.jpg" },
+  { name: "Darren Sammy", country: "West Indies", debut: 2007, retirement: 2016, age: 41, img: "images/sammy.jpg" },
+  
+];
 
 let currentIndex = 0;
 const total = players.length;
